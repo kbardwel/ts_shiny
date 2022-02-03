0074fd134ce415866789bcad9120fdcf8502f98a
kbardwel <98776417+kbardwel@users.noreply.github.com>
2022-02-03 09:14:53 -0500

    create mvp

--- server.R
+++ server.R
@@ -1,10 +1,10 @@
 library(shiny)
-library(fpp3)
-
-sales_df <- read.csv("data/clean_sales.csv")
-sales_df$date <- as.Date(sales_df$date)
-sales_df <- as_tsibble(sales_df, key = c("scode", "pcode"), index = "date")
 
 server <- function(input, output) {
 
+output$ts_plot <- renderPlot({
+  plot_df <- sales_df[sales_df$scode == input$selected_store & sales_df$pcode == input$selected_sku,]
+
+  autoplot(plot_df)
+  })
 }
--- ui.R
+++ ui.R
@@ -1,5 +1,15 @@
 library(shiny)
 
 ui <- fluidPage(
+  selectInput(
+    inputId = 'selected_sku', label = "Select SKU:",
+    choices = unique(sales_df$pcode)
+  ),
+  selectInput(
+    inputId = 'selected_store', label = "Select Store:",
+    choices = unique(sales_df$scode)
+  ),
+  plotOutput("ts_plot"),
+  verbatimTextOutput("debug")
 
 )
